bbed21d06869f585b56aea71da11412478d5dd0c
Nicholas Patterson <nicholaspatterson@Nicholass-MBP.home>
2019-07-22 18:57:52 -0400

    Fixed my movie component

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -2,29 +2,28 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const Movie = props => {
-  const [movie, setMovie] = useState({});
-  const [stars, setStars] = useState([]);
+  const [movie, setMovie] = useState(null);
+
   useEffect(() => {
     const id = props.match.params.id;
 
     axios.get(`http://localhost:5000/api/movies/${id}`).then(response => {
       setMovie(response.data);
-      setStars(response.data.stars);
     });
   }, []);
 
   // Uncomment this only when you have moved on to the stretch goals
-  const saveMovie = props => {
-    console.log(props);
-    const addToSavedList = props.addToSavedList;
-    addToSavedList(movie);
-  };
+  // const saveMovie = props => {
+  //   console.log(props);
+  //   const addToSavedList = props.addToSavedList;
+  //   addToSavedList(movie);
+  // };
 
   if (!movie) {
     return <div>Loading movie information...</div>;
   }
 
-  const { title, director, metascore } = movie;
+  const { title, director, metascore, stars } = movie;
 
   return (
     <div className="save-wrapper">
